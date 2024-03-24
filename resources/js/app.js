ea12dfe1c0a9540f75a2bd9f368b46d22ae03cf1
dev-salman <isalman.developer@gmail.com>
2024-03-25 02:42:00 +0500

    welcome page, vite and adding assets
    we create the first page and copy all of the html from admin-lte live page
    
    using laravel Asset Bundling (Vite) to bundle all of the js and css files in one file
    
    for images using the live link to load them

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+/*
+importing these file from admin-lte package as we have installed them through npm and they are all imported here just because of @vite to bundle them in a single file for fast loading
+*/
+
 import './bootstrap';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
