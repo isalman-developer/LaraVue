42011168688d2c1f23f21b36c34a7dc7e913eccc
dev-salman <isalman.developer@gmail.com>
2024-06-01 18:19:12 +0500

    deleting bulk users check box
    deleting bulk users by using checkboxes for each row and then also updating the existing users list

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::post('/api/users', [UserController::class, 'store']);
 Route::patch('/api/users/{user}/change-role', [UserController::class, 'changeRole']);
 Route::put('/api/users/{user}', [UserController::class, 'update']);
 Route::delete('/api/users/{user}', [UserController::class, 'destroy']);
+Route::delete('/api/users', [UserController::class, 'bulkDelete']);
 
 // adding a condition to take anything as url and load the default view welcome.blade.php & this route must be at the end always
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
