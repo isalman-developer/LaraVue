76d3c28953901f3f84991e685968ae8719eb29cf
dev-salman <isalman.developer@gmail.com>
2024-07-03 17:33:02 +0500

    showing appointments kips on dashboard
    creating new api and on change of appointment status call the api and fetch the count of appointments (all, cancelled, confirmed, scheduled)
    
    also call the api on onMounted so be default the count of all of the appointments has to be fetched

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-##  Authentication error handling
+## Dynamic Dashboard KPIS
 
-1. adding error design
+1. created 2 ref variable `const selectedAppointmentsCount = ref(0)` and `const selectedAppointmentSatatus = ref('all')`
 
-2. adding loading to sign in button until request is resolved
+2. add `@change` event to select (all, scheduled, confirmed, cancelled). to call __getAppointmentsCount__ to fetch count of appointments
 
-3. also add an errorMessage and loading ref variable which will be used for loading, disabling sing in button and showing error 
+3. add `getAppointmentsCount` for bringing back the count of appointments (all, scheduled, cancelled or confirmd appointments).
+
+4. also call __getAppointmentsCount__ function on `onMounted` so when the component is loaded then the count of all of the components will be displaye.
+
--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,24 +1,99 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const selectedAppointmentSatatus = ref('all');
+const selectedAppointmentsCount = ref(0);
+
+const getAppointmentsCount = () => {
+
+    axios.get('/api/stats/appointments', {
+        params: {
+            status: selectedAppointmentSatatus.value
+        }
+    }).then((response) => {
+        selectedAppointmentsCount.value = response.data.totalAppointmentsCount;
+    })
+}
+
+onMounted(() => {
+    getAppointmentsCount();
+})
+</script>
+
 <template>
-   <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <div class="d-flex justify-content-between">
+                                <h3>{{ selectedAppointmentsCount }}</h3>
+                                <select v-model="selectedAppointmentSatatus"
+                                    @change="getAppointmentsCount($event.target.value)"
+                                    style="height: 2rem; outline: 2px solid transparent;" class="px-1 rounded border-0">
+                                    <option value="all">All</option>
+                                    <option value="scheduled">Scheduled</option>
+                                    <option value="confirmed">Confirmed</option>
+                                    <option value="cancelled">Cancelled</option>
+                                </select>
+                            </div>
+                            <p>Appointments</p>
                         </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard</li>
-                            </ol>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
                         </div>
+                        <router-link :to="{ name: 'admin.appointments' }" class="small-box-footer">
+                            View Appointments
+                            <i class="fas fa-arrow-circle-right"></i>
+                        </router-link>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <div class="d-flex justify-content-between">
+                                <h3>0</h3>
+                                <select style="height: 2rem; outline: 2px solid transparent;"
+                                    class="px-1 rounded border-0">
+                                    <option value="TODAY">Today</option>
+                                    <option value="30">30 days</option>
+                                    <option value="60">60 days</option>
+                                    <option value="360">360 days</option>
+                                    <option value="MTD">Month to Date</option>
+                                    <option value="YTD">Year to Date</option>
+                                </select>
+                            </div>
+                            <p>Users</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <router-link :to="{ name: 'admin.users' }" class="small-box-footer">
+                            View Users
+                            <i class="fas fa-arrow-circle-right"></i>
+                        </router-link>
                     </div>
                 </div>
             </div>
-
-
-            <div class="content">
-                <div class="container-fluid">
-
-                </div>
-            </div>
+        </div>
+    </div>
 </template>
