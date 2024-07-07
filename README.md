448fe12b52e034a17b1e87dfa947c8e1b5bdb24f
dev-salman <isalman.developer@gmail.com>
2024-07-07 22:57:02 +0500

    Dynamic Settings of app using crud database

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-## Adding data to localStorage (storing state of menu expended or collapsed)
+## Application Settings (Dynamic properties of app)
+1. How to create some dynamic properties of an application (app_name, date_formate, pagination_limit etc).
 
-Code added in welcome.blade.php under comment 
-//code added for stoting the state of side menu in local storage so after refreshing the page the state of the side menu should remain the same
+2. We will create a table for it, to store it and also update them if needed. (Model, Migration).
 
-In this commit we actually performed an action of storing a value in localStorage and then getting that value, this can be used to maintain the state of a sidebar to be expended or callapsed event after a page refresh.
+3. Create a seeder for it, to store default values to these columns.
+
+4. In __UpdateSetting.vue__ create a function `getSettings`to fetch settings values from databse. also call `getSettings` on `onMounted`;
+
+5. Create a `const settings = ref([]);` to store the value of the response and also populate the form inputs. 
+
+6. Create a __route__ and a controller __SettingController__ for this functionality.
+
+7. Now we have to update settings so create a new function `updateSettings` with __put axios request__ & create an `api` for this function.
+
+8. Add __toastr__ for update success function.
+
+## Add validation for this in Controller and display them on vue also
+
+1. validate the settings values, if there is any error store them in `const errors = ref()` and then display them conditionally under each field like i.e
+    ```
+      <span class="text-danger text-sm" v-if="errors && errros.app_name" >{{ errors.app_name[0] }}</span>
+    ```
+2. on calling `updateSettings` clear the errors so they they will be disappeared from errors.value and new value will be inserted.
--- resources/js/pages/settings/UpdateSetting.vue
+++ resources/js/pages/settings/UpdateSetting.vue
@@ -1,3 +1,37 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import useToastr from '../../toastr';
+
+const settings = ref([]);
+const toastr = useToastr();
+const errors = ref();
+
+const getSettings = () => {
+    axios.get('/api/settings')
+        .then((response) => {
+            settings.value = response.data;
+        })
+};
+
+const updateSetting = () => {
+    errors.value = '';
+    axios.put('/api/settings', settings.value)
+        .then((response) => {
+            toastr.success(response.data.success);
+        }).catch((error) => {
+            if (error && error.response.status == 422) {
+                errors.value = error.response.data.errors;
+            }
+        });
+}
+
+onMounted(() => {
+    getSettings();
+});
+
+</script>
+
 <template>
     <div class="content-header">
         <div class="container-fluid">
@@ -18,7 +52,56 @@
 
     <div class="content">
         <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">General Setting</h3>
+                        </div>
 
+                        <form @submit.prevent="updateSetting()">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="appName">App Display Name</label>
+                                    <input v-model="settings.app_name" type="text" class="form-control" id="appName"
+                                        placeholder="Enter app display name">
+                                    <span class="text-danger text-sm" v-if="errors && errors.app_name">
+                                        {{ errors.app_name[0] }}
+                                    </span>
+                                </div>
+                                <div class="form-group">
+                                    <label for="dateFormat">Date Format</label>
+                                    <select v-model="settings.date_format" class="form-control">
+                                        <option value="MM/DD/YYYY">MM/DD/YYYY</option>
+                                        <option value="DD/MM/YYYY">DD/MM/YYYY</option>
+                                        <option value="YYYY-MM-DD">YYYY-MM-DD</option>
+                                        <option value="Month DD, YYYY">Month DD, YYYY</option>
+                                        <option value="DD Month YYYY">DD Month YYYY</option>
+                                    </select>
+                                    <span class="text-danger text-sm" v-if="errors && errors.date_format">
+                                        {{ errors.date_format[0] }}
+                                    </span>
+
+                                </div>
+                                <div class="form-group">
+                                    <label for="paginationLimit">Pagination Limit</label>
+                                    <input v-model="settings.pagination_limit" type="text" class="form-control"
+                                        id="paginationLimit" placeholder="Enter pagination limit">
+                                    <span class="text-danger text-sm" v-if="errors && errors.pagination_limit">
+                                        {{ errors.pagination_limit[0] }}
+                                    </span>
+
+                                </div>
+                            </div>
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary"><i class="fa fa-save mr-1"></i>Save
+                                    Changes</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
